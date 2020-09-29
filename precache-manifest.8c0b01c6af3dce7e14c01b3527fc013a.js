82f65823409099f48ccd603a4e1fb5eed95eb20e
Nazar <nazshved@gmail.com>
2020-09-29 18:45:16 +0300

    Updates

---
+++ precache-manifest.8c0b01c6af3dce7e14c01b3527fc013a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c0e01a0dfcf317c7126ca792b81b36",
+    "url": "/react_3/index.html"
+  },
+  {
+    "revision": "5e7cebd78c1001704623",
+    "url": "/react_3/static/css/main.e4f7fa56.chunk.css"
+  },
+  {
+    "revision": "0ac5ff9281cd46b10537",
+    "url": "/react_3/static/js/2.97a4592b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_3/static/js/2.97a4592b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7cebd78c1001704623",
+    "url": "/react_3/static/js/main.54d391c3.chunk.js"
+  },
+  {
+    "revision": "8f30d84b4ab4cb2020d5",
+    "url": "/react_3/static/js/runtime-main.b483ad71.js"
+  }
+]);
